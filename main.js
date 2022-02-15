34a7b08874bf36eef93f86cd8c425f6f360aa9a7
Maciej Marchewka <basicuser00@protonmail.com>
2022-02-15 19:54:17 +0100

    m

--- main.js
+++ main.js
@@ -77,6 +77,28 @@ document.querySelectorAll(".tech-logo").forEach(function(logo) {
     });
   });
 
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelector('.menu__item');
+const hamburger = document.querySelector('.hamburger-btn');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+
+
 
  
 
